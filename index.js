10f2b7c2c4fed9cf0b154c79f5f5486d2a5eadc0
Sullyvan Ogawa <sullyvanogawa@gmail.com>
2026-02-15 18:29:53 -0300

    alterando o valor da porta para 3001, para evitar conflito

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 import express from 'express';
 
 const host = "0.0.0.0"; // requisições podem vir de todas as interfaces do host local 
-const porta = 3000; // identifica única e exclusivamente uma apliação neste host
+const porta = 3001; // identifica única e exclusivamente uma apliação neste host
 
 const app = express();
 
 //Aqui todo o conteúdo do diretório Views/public estará disponível na raiz do servidor
-app.use(express.static('./Views/public'));
+app.use(express.static('views/public'));
 
 app.listen(porta, host, () => {
     console.log(`Servidor rodando em http://${host}:${porta}`);
